8dc6c6d00f7e25a290944fd3ecc8feed306572fb
Panajotc <panajot7@gmail.com>
2021-11-16 11:42:44 +0100

    medical

---
+++ database/migrations/2021_11_16_103537_create_fattura_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFatturaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fattura', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("cogname");
+            $table->integer("code_di_fiscale");
+            $table->string("email");
+            $table->string("Indirizzo");
+            $table->string("citta");
+            $table->integer("Cap");
+            $table->string("Indirizzo");
+            $table->string("Nazione");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fattura');
+    }
+}
