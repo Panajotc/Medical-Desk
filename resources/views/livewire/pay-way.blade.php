02f3efa8594450c5f97c2ef912e9328e8ff11f78
Panajotc <panajot7@gmail.com>
2021-11-26 11:48:42 +0100

    fixes

--- resources/views/livewire/pay-way.blade.php
+++ resources/views/livewire/pay-way.blade.php
@@ -3,8 +3,8 @@
 <div id="pay-way" class="mb-3">
           
           <div class="mb-3">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-            <label class="form-check-label" for="inlineRadio1">{{$text1}}</label>
+            <input class="form-check-input" type="checkbox">
+            <label class="form-check-label">{{$text1}}</label>
           </div>
           
         </div>
