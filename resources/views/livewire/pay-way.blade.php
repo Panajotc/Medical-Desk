b891f4066283dd4dd05063a4d11fcc302db0c21e
Panajotc <panajot7@gmail.com>
2021-11-26 22:11:00 +0100

    fixes

--- resources/views/livewire/pay-way.blade.php
+++ resources/views/livewire/pay-way.blade.php
@@ -2,22 +2,12 @@
 
 <div id="pay-way" class="mb-3">
           
-          <div class="mb-3">
+          <div class="mb-3 ">
             <input class="form-check-input" type="checkbox">
-            <label class="form-check-label">{{$text1}}</label>
+            <label class="form-check-label pl-6">{{$text1}}</label>
           </div>
           
-        </div>
+</div>
 
 
-        <style>
-        #gender .mb-3 {
-           display:inline-flex;
-          
-        }
-
-        label{
-            padding-left:27px;
-            
-        }
-        </style>
+        
